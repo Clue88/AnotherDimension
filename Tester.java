5a4b7fff41cff6c7dbed24c0ae983eba85111b4e
Christopher Liu <christopherliu@sorcero.com>
2020-10-22 14:17:59 -0400

    Add largest method

--- Tester.java
+++ Tester.java
@@ -1,6 +1,9 @@
 public class Tester {
     public static void main(String[] args) {
-        int[] test1 = {1, 2, 3, 5};
+        int[] test1 = {};
+        int[] test2 = {1, 2, 3, 5};
         System.out.println(ArrayOps.sum(test1));
+        System.out.println(ArrayOps.sum(test2));
+        System.out.println(ArrayOps.largest(test2));
     }
 }
