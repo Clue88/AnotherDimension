4c37c94308a9468a929c293cbc643246e23ab487
Christopher Liu <christopherliu@sorcero.com>
2020-10-22 15:05:53 -0400

    Add more tests

--- Tester.java
+++ Tester.java
@@ -5,20 +5,32 @@ public class Tester {
         int[] test2 = {1, 2, 3, 5};
         int[] test3 = {5, 2, 12, 10, 4};
         int[][] test4 = {test2, test3};
+        int[][] A = {
+            {1, 0, 12, -1},
+            {7, -2, 2, 1},
+            {-5, -2, 2, -9}
+        };
+        int[] B = {1, 3, 5};
 
         System.out.println(ArrayOps.sum(test1)); // should be 0
         System.out.println(ArrayOps.sum(test2)); // should be 11
         System.out.println(ArrayOps.sum(test3)); // should be 33
+        System.out.println(ArrayOps.sum(B)); // should be 9
 
         System.out.println(ArrayOps.largest(test2)); // should be 5
         System.out.println(ArrayOps.largest(test3)); // should be 12
 
         System.out.println(Arrays.toString(ArrayOps.sumRows(test4)));
         // should be [11, 33]
+        System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
+        // should be [12, 8, -14]
 
         System.out.println(Arrays.toString(ArrayOps.largestInRows(test4)));
         // should be [5, 12]
+        System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
+        // should be [12, 7, 2]
 
         System.out.println(ArrayOps.sum(test4)); // should be 44
+        System.out.println(ArrayOps.sum(A)); // should be 6
     }
 }
